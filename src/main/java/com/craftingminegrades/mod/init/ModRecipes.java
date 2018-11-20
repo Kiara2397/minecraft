2c773d2349f0859314a243bd234d0884654afc01
Kiara <Kiara@LAPTOP-0C6464DU>
2018-11-19 22:53:34 -0500

    Added smelting recipe for Vibranium(Item Phase1) using Vibranium Ore

--- src/main/java/com/craftingminegrades/mod/init/ModRecipes.java
+++ src/main/java/com/craftingminegrades/mod/init/ModRecipes.java
@@ -7,5 +7,6 @@ public class ModRecipes {
 
 	public static void init() {
 		GameRegistry.addSmelting(ModBlocks.AMETHYST_ORE, new ItemStack(ModItems.AMETHYST, 1), 1F);
+		GameRegistry.addSmelting(ModBlocks.VIBRANIUM_ORE, new ItemStack(ModItems.VIBRANIUM, 1), 2F);
 	}
 }
