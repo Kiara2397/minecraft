4ab3d8c7517232a20545cea03b62bb3361f750dc
Kiara <Kiara@LAPTOP-0C6464DU>
2018-11-19 21:27:35 -0500

    Created smelting recipe for Amethyst gem using Amethyst Ore

--- src/main/java/com/craftingminegrades/mod/init/ModItems.java
+++ src/main/java/com/craftingminegrades/mod/init/ModItems.java
@@ -12,5 +12,5 @@ public class ModItems {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
 	public static final Item VIBRANIUM = new ItemBase("vibranium");
-
+	public static final Item AMETHYST = new ItemBase("amethyst");
 }
--- src/main/java/com/craftingminegrades/mod/init/ModRecipes.java
+++ src/main/java/com/craftingminegrades/mod/init/ModRecipes.java
@@ -1,9 +1,11 @@
 package com.craftingminegrades.mod.init;
 
-public class ModRecipes {
-	
-	public static void init() {
-		
-	}
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
+public class ModRecipes {
+
+	public static void init() {
+		GameRegistry.addSmelting(ModBlocks.AMETHYST_ORE, new ItemStack(ModItems.AMETHYST, 1), 1F);
+	}
 }
