39a790b35ce48676fee20d499a1be63a9070b9e7
Fernando Gonzalez Fuentes <fernando.gonzalez7@upr.edu>
2018-11-21 00:42:28 -0400

    Added Tools

--- src/main/java/com/craftingminegrades/mod/init/ModItems.java
+++ src/main/java/com/craftingminegrades/mod/init/ModItems.java
@@ -4,13 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.craftingminegrades.mod.items.ItemBase;
+import com.craftingminegrades.mod.items.tools.ToolAxe;
+import com.craftingminegrades.mod.items.tools.ToolHoe;
+import com.craftingminegrades.mod.items.tools.ToolPickaxe;
+import com.craftingminegrades.mod.items.tools.ToolSpade;
+import com.craftingminegrades.mod.items.tools.ToolSword;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemAxe;
+import net.minecraft.item.ItemHoe;
+import net.minecraft.item.ItemPickaxe;
+import net.minecraft.item.ItemSpade;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.common.util.EnumHelper;
 
 public class ModItems {
-	
+
 	public static final List<Item> ITEMS = new ArrayList<Item>();
-	
+
+	//Materials
+	public static final ToolMaterial MATERIAL_VIBRANIUM = EnumHelper.addToolMaterial("material_vibranium", 3, 2000, 9.0F, 5.0F, 30);
+
+	//Items
 	public static final Item VIBRANIUM = new ItemBase("vibranium");
 	public static final Item AMETHYST = new ItemBase("amethyst");
+
+	//Tools
+	public static final ItemSword VIBRANIUM_SWORD = new ToolSword("vibranium_sword", MATERIAL_VIBRANIUM);
+	public static final ItemSpade VIBRANIUM_SHOVEL = new ToolSpade("vibranium_shovel", MATERIAL_VIBRANIUM);
+	public static final ItemPickaxe VIBRANIUM_PICKAXE = new ToolPickaxe("vibranium_pickaxe", MATERIAL_VIBRANIUM);
+	public static final ItemAxe VIBRANIUM_AXE = new ToolAxe("vibranium_axe", MATERIAL_VIBRANIUM);
+	public static final ItemHoe VIBRANIUM_HOE = new ToolHoe("vibranium_hoe", MATERIAL_VIBRANIUM);
 }
---
+++ src/main/java/com/craftingminegrades/mod/items/tools/ToolSpade.java
@@ -0,0 +1,27 @@
+package com.craftingminegrades.mod.items.tools;
+
+import com.craftingminegrades.mod.Main;
+import com.craftingminegrades.mod.init.ModItems;
+import com.craftingminegrades.mod.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemSpade;
+import net.minecraft.item.Item.ToolMaterial;
+
+public class ToolSpade extends ItemSpade implements IHasModel {
+	
+	public ToolSpade(String name, ToolMaterial material) {
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.MATERIALS);
+
+		ModItems.ITEMS.add(this);
+	}
+
+	@Override
+	public void registerModels() {
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+
+	}
+}
