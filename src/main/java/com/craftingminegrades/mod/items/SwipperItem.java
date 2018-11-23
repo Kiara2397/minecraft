36527961ba6e0ff2f3ff5f1d35068fc0c9f3f736
Kiara <Kiara@LAPTOP-0C6464DU>
2018-11-23 16:45:40 -0500

    Created class for Main Feature: Swipper Item

---
+++ src/main/java/com/craftingminegrades/mod/items/SwipperItem.java
@@ -0,0 +1,12 @@
+package com.craftingminegrades.mod.items;
+
+import com.craftingminegrades.mod.util.IHasModel;
+
+public class SwipperItem extends ItemBase implements IHasModel{
+
+	public SwipperItem(String name) {
+		super(name);
+
+	}
+
+}
