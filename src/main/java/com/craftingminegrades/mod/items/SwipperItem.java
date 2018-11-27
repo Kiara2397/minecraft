f508ee712982f08d6394c3daa942579484764672
Fernando Gonzalez Fuentes <fernando.gonzalez7@upr.edu>
2018-11-27 01:07:18 -0400

    Added consumption mode

--- src/main/java/com/craftingminegrades/mod/items/SwipperItem.java
+++ src/main/java/com/craftingminegrades/mod/items/SwipperItem.java
@@ -16,6 +16,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemCoal;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
@@ -27,44 +28,44 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 public class SwipperItem extends ItemBase {
-	
+
 	private int usages = 0;
 	private static String mode = "Swapping";
 	private IBlockState blockSelected = Blocks.AIR.getDefaultState();
 	private String blockSelectedName = "None";
-	
-	
+
+
 	public SwipperItem(String name) {
 		super(name);
 	}
-	
+
 	//Adds information to tooltip
 	public void addInformation(ItemStack stack, @Nullable World world, List<String> list, ITooltipFlag b) {
 		super.addInformation(stack, world, list, b);
 		list.add(TextFormatting.DARK_PURPLE + I18n.format("This item swaps the block you're looking at for another selected block."));
-		list.add(TextFormatting.DARK_PURPLE + I18n.format("The item charges with ingots:"));
-		list.add(TextFormatting.DARK_PURPLE + I18n.format("Gold +10 usages"));
-		list.add(TextFormatting.DARK_PURPLE + I18n.format("Iron +5 usages"));
+		list.add(TextFormatting.DARK_PURPLE + I18n.format("The item charges with coal:"));
+		list.add(TextFormatting.DARK_PURPLE + I18n.format("1 coal = + 5 usages"));
 		list.add(TextFormatting.YELLOW + I18n.format("One usage per swap."));
-		list.add(TextFormatting.GREEN + I18n.format("Right-Click to swap the block you're looking at."));
+		list.add(TextFormatting.GREEN + I18n.format("When in Swapping mode, Right-Click to swap the block you're looking at."));
 		list.add(TextFormatting.GREEN + I18n.format("Shif + Right-Click to select the block you're looking at."));
-		list.add(TextFormatting.GREEN + I18n.format("Press '"+RegistryHandler.getChangeModeLetter()+"' to change modes."));
+		list.add(TextFormatting.RED + I18n.format("When in Charging mode, Right-Click to use one of your coals and charge the usages"));
+		list.add(TextFormatting.DARK_BLUE + I18n.format("Press '"+RegistryHandler.getChangeModeLetter()+"' to change modes."));
 		list.add(TextFormatting.AQUA + I18n.format("Mode: " + mode));
 		list.add(TextFormatting.AQUA + I18n.format("Usages: " + usages));
 		list.add(TextFormatting.AQUA + I18n.format("Block selected: " + blockSelectedName));
-		
+
 	}
-	
+
 	public static void setMode(String mode) {
 		SwipperItem.mode = mode;
 	}
 
-	
+
 	@Override
 	public boolean canItemEditBlocks() {
 		return true;
 	}
-	
+
 	@Override
 	public Item setMaxStackSize(int maxStackSize) {
 		maxStackSize = 1;
@@ -74,6 +75,11 @@ public class SwipperItem extends ItemBase {
 	@Override
 	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {		
 		if(!worldIn.isRemote) {
+
+			//Variables for item search and reduction
+			ItemStack fuelItem = this.findFuel(player);
+			boolean hasCoal = (fuelItem.getItem() instanceof ItemCoal);
+
 			//Selecting Block for swap
 			if(player.isSneaking()) {
 				blockSelected = worldIn.getBlockState(pos);
@@ -81,19 +87,65 @@ public class SwipperItem extends ItemBase {
 				Minecraft.getMinecraft().player.sendMessage(new TextComponentString("Block Selected: "+blockSelectedName));
 			}
 
-			//Swapping
+			//Modes
 			if(Minecraft.getMinecraft().player.getHeldItemMainhand().getItem() instanceof SwipperItem) {
+
+				//Swapping
 				if(mode.equals("Swapping")) {
 					if(usages>0) {
 						worldIn.destroyBlock(pos, true);
 						worldIn.setBlockState(pos, blockSelected,1);
 						return EnumActionResult.SUCCESS;
 					}
-					Minecraft.getMinecraft().player.sendMessage(new TextComponentString("Usage must be greater than 0. Must charge item."));
+					Minecraft.getMinecraft().player.sendMessage(new TextComponentString("Usage must be greater than 0. Find Coal and charge!"));
+				}
+				//Charging
+				if(mode.equals("Charging")) {
+					if(hasCoal) {
+						fuelItem.shrink(1);
+						if (fuelItem.isEmpty()) {
+							player.inventory.deleteStack(fuelItem);
+						}
+						usages = usages + 5;
+					}
 				}
 			}
+
 		}
 		return EnumActionResult.PASS;
+	}
+
+	//Checks if is fuel(coal)
+	protected boolean isFuel(ItemStack stack)
+	{
+		return stack.getItem() instanceof ItemCoal;
+	}
+
+	//Finds coal
+	private ItemStack findFuel(EntityPlayer player)
+	{
+		if (this.isFuel(player.getHeldItem(EnumHand.OFF_HAND)))
+		{
+			return player.getHeldItem(EnumHand.OFF_HAND);
+		}
+		else if (this.isFuel(player.getHeldItem(EnumHand.MAIN_HAND)))
+		{
+			return player.getHeldItem(EnumHand.MAIN_HAND);
+		}
+		else
+		{
+			for (int i = 0; i < player.inventory.getSizeInventory(); ++i)
+			{
+				ItemStack itemstack = player.inventory.getStackInSlot(i);
+
+				if (this.isFuel(itemstack))
+				{
+					return itemstack;
+				}
+			}
+
+			return ItemStack.EMPTY;
+		}
 	}
 
 	@Override
@@ -106,7 +158,7 @@ public class SwipperItem extends ItemBase {
 	public int getItemBurnTime(ItemStack itemStack) {
 		return super.getItemBurnTime(itemStack);
 	}
-	
+
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
 		return super.onItemRightClick(worldIn, playerIn, handIn);
